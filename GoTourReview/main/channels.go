6feb769babb6ea0572938258293fde7efd54e38f
CrowHyc <huangyisi_cd@shishike.com>
2018-02-26 17:11:53 +0800

    [Add] add GoTour review goroutine and channels

---
+++ GoTourReview/main/channels.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func sum(a []int, c chan int) {
+	sum := 0
+	for _, v := range a {
+		sum += v
+	}
+	c <- sum
+}
+
+func main() {
+	a := []int{7, 8, 9, 0, 3, 5, -9}
+	c := make(chan int)
+	go sum(a[:len(a)/2], c)
+	go sum(a[len(a)/2:], c)
+	x, y := <-c, <-c
+	fmt.Println(x)
+	fmt.Println(y)
+}
